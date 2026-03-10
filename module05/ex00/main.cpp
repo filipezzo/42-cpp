6fa42c15ca67a287fa6d8ade1f87dc8c3803dcc0
filipezzo <fastnexi@gmail.com>
2026-03-10 13:42:33 -0300

    feat: ex00 test

--- module05/ex00/main.cpp
+++ module05/ex00/main.cpp
@@ -1,8 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsousa <fsousa@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/10 13:41:33 by fsousa            #+#    #+#             */
+/*   Updated: 2026/03/10 13:41:33 by fsousa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Bureaucrat.hpp"
 
 int main(void)
 {
-    Bureaucrat b("filipe", 150);
-    std::cout << b << std::endl;
+    try
+    {
+        Bureaucrat a("Filipe", 42);
+        std::cout << a << std::endl;
+        for(int i = 0; i < 30; i++)
+            a.incrementGrade();
+         std::cout << a << std::endl;
+         Bureaucrat b("Enzo", 0);
+        std::cout << b << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
     return 0;
+    
 }
